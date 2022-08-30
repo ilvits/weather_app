d5108af2f7401bd7c4fdad2dc701fb5ff1912d9f
ILViTs <60173598+ilvits@users.noreply.github.com>
2022-08-31 02:03:09 +0300

    redesign UI

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,68 +1,80 @@
-let mainContainer
-const weatherPanteleyki = document.getElementById('weather-panteleyki')
-const weatherTorrevieja = document.getElementById('weather-torrevieja')
-const coordsPanteleyki = weatherPanteleyki.dataset.gps
-const coordsTorrevieja = weatherTorrevieja.dataset.gps
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+var loc = JSON.stringify(
+    {
 
-weatherPanteleyki.addEventListener('click', function onClick(e) {
-    e.preventDefault;
-    getWeather('panteleyki')
-})
-weatherTorrevieja.addEventListener('click', function onClick(e) {
-    e.preventDefault
-    getWeather('torrevieja')
-})
-document.addEventListener("DOMContentLoaded", ready);
+        "panteleyki": { "name": "Пантелейки", "latitude": 55.6743, "longitude": 27.0192 },
+        "torrevieja": { "name": "Торревьеха", "latitude": 37.9815, "longitude": -0.6753 }
 
-function ready() {
-    getWeather('panteleyki')
-    setTimeout(() => getWeather('torrevieja'), 1000);
+    }
+);
+
+setCookie('locations', loc)
+var locations = JSON.parse(decodeURIComponent(getCookie('locations')));
+console.log(locations.panteleyki);
+
+document.addEventListener("DOMContentLoaded", refreshWeatherData);
+
+function refreshWeatherData() {
+    getWeather(locations.panteleyki)
 }
 
 function getWeather(city) {
-    switch (city) {
-        case 'torrevieja':
-            var coordinates = coordsTorrevieja
-            condition = document.getElementById("condition-torrevieja");
-            break;
-        case 'panteleyki':
-            var coordinates = coordsPanteleyki
-            condition = document.getElementById("condition-panteleyki");
-            break;
-    }
+    let latitude = city.latitude
+    let longitude = city.longitude
+    condition = document.getElementById("condition-torrevieja");
+
     const options = {
         method: 'GET',
         headers: { Accept: 'application/json', 'Accept-Encoding': 'gzip' }
     };
-    const url = `https://api.openweathermap.org/data/2.5/weather?${coordinates}&lang=ru&appid=3cbb42cb4e56223f9424b9d02c75cc36&units=metric`
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude}%2C${longitude}?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslikemin%2Cfeelslike%2Chumidity%2Cprecip%2Cprecipprob%2Cwindspeed%2Cwinddir%2Cpressure%2Cconditions%2Cdescription%2Cicon&include=fcst%2Cobs%2Cremote%2Cstatsfcst%2Cstats%2Chours%2Calerts%2Cdays%2Ccurrent&key=6M44EU7ZDRK49GFJHKBCX2JJC&contentType=json`
     fetch(url, options)
         .then(response => response.json())
-        .then(weatherData => appendData(city, weatherData))
+        .then(weatherData => appendData(weatherData))
         .catch(err => console.log(err));
 }
 
-function appendData(city, weatherData) {
+function appendData(weatherData) {
     console.log(weatherData);
-    condition.innerHTML = `
-    <img class="w-12 h-12 m-4 bg-gradient-to-r from-green-400 to-blue-500 rounded-full" src="http://openweathermap.org/img/wn/${weatherData['weather']['0']['icon']}@2x.png">
-    <div class="text-lg">${weatherData['weather']['0']['description']}</div>`
-    for (const [key, value] of Object.entries(weatherData.main)) {
-        let id = `${city}-${key}`
-        console.log(id)
-        try {
-            let container = document.getElementById(id);
-            if ((key == 'temp' || key == 'feels_like') && value < 20) {
-                container.classList.add('text-blue-500')
-            } else if ((key == 'temp' || key == 'feels_like') && value < 25) {
-                container.classList.add('text-black')
-            } else if ((key == 'temp' || key == 'feels_like') && value < 35) {
-                container.classList.add('text-orange-500')
-            } else if ((key == 'temp' || key == 'feels_like') && value < 45) {
-                container.classList.add('text-red-500')
-            }
-            container.innerHTML = Math.ceil(value)
-        }
-        catch (error) { console.log(error) }
+
+    document.getElementById('temperature').innerHTML = Math.ceil(weatherData.currentConditions.temp)
+    document.getElementById('humidity').innerHTML = Math.ceil(weatherData.currentConditions.humidity)
+    document.getElementById('feelslike').innerHTML = Math.ceil(weatherData.currentConditions.feelslike)
+    document.getElementById('windspeed').innerHTML = Math.ceil(weatherData.currentConditions.windspeed)
+    var style = document.createElement('style');
+    var keyFrames = `
+    #arrow {
+        animation: rotate 1s;
     }
-    document.getElementById(`${city}-wind`).innerHTML = `${weatherData.wind.speed} `
+    @keyframes rotate {
+         from {
+             transform: rotate(0deg);
+         }
+         to {
+             transform: rotate(${Math.ceil(weatherData.currentConditions.winddir)}deg);
+         }
+     }`;
+    style.innerHTML = keyFrames;
+    document.getElementsByTagName('head')[0].appendChild(style);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
@@ -9,6 +10,26 @@ module.exports = {
       fontFamily: {
         sans: ['Hauora'],
       },
+    },
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
+      blue: colors.blue,
+      slate: colors.slate,
+      'bg': '#011839',
+      'cyan': '#3FD5FE',
+      'blue': '#0090C3',
+      'yellow': '#FEB800',
+      primary: colors.indigo,
+      secondary: colors.yellow,
+      neutral: colors.gray,
+      transparent: 'transparent',
+      current: 'currentColor',
     },
     plugins: [],
   }
