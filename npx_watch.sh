5fa82b998ee24105bac0420ea5f5ac505afa15af
ILViTs <60173598+ilvits@users.noreply.github.com>
2023-02-24 00:36:34 +0400

    bug fixes

--- npx_watch.sh
+++ npx_watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/projects/sandbox/weather_app/
-npx tailwindcss -i ./src/css/style.css -o ./weather/css/style.css --watch
+npx tailwindcss -i ./src/css/style.css -o ./css/style.css --watch
